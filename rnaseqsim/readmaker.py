10170428c5a21005fabf30d81da4fa24f196c3d8
mslilyb <lilith.r.m.d@gmail.com>
2025-03-19 15:01:43 -0700

    start but not very far considering the RNA seq sim

---
+++ rnaseqsim/readmaker.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+"""
+Want to generate 'mature' mRNA transcripts from our simulated isoforms, then fragment them (how does that work?). Amplification before or after fragments?
+
+Then, align our library to reference, see what reads go where and how our results compare to our expected abundances.
+"""
