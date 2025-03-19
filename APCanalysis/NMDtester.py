5e71d7fa7e032000c6f76efe6f1bdb9ca38a21cc
carl <icacedo@ucdavis.edu>
2025-03-18 23:01:44 -0700

    up

--- APCanalysis/NMDtester.py
+++ APCanalysis/NMDtester.py
@@ -1,6 +1,5 @@
 import argparse
-import re
-import sys
+import glob
 
 import isoform2
 from grimoire.genome import Reader
@@ -12,8 +11,9 @@ from grimoire.genome import Reader
 
 parser = argparse.ArgumentParser(description='Does nmd-ish improve APC predictions?')
 parser.add_argument('model', help='splice model file')
-parser.add_argument('fasta', help='fasta file')
-parser.add_argument('gff', help='gff file')
+#parser.add_argument('fasta', help='fasta file')
+#parser.add_argument('gff', help='gff file')
+parser.add_argument('smallgenes', help='smallgenes directory')
 parser.add_argument('--min-orf', type=int, default=25,
 	help='minimum distance from start to stop [%(default)i]')
 parser.add_argument('--nonstop', type=float, default=1e-3,
@@ -31,53 +31,76 @@ parser.add_argument('--flank', type=int, default=99,
 
 args = parser.parse_args()
 
-model = isoform2.read_splicemodel(args.model)
-reader = Reader(fasta=args.fasta, gff=args.gff)
-region = next(reader)
-locus = isoform2.Locus(region.name, region.seq, model, limit=args.limit)
+def compare_dists(model, infasta, ingff, inlimit):
 
-with open('prenmdish.gff.tmp', 'w') as fp:
-	locus.write_gff(fp)
-			
-gene = region.ftable.build_genes()[0]
-txs = gene.transcripts()
-atgs = set()
-for tx in txs:
-	cdss = sorted(tx.cdss, key=lambda x: x.beg)
-	atgs.add(cdss[0].beg -1)
-cds_beg = sorted(list(atgs))[0]
+	model = isoform2.read_splicemodel(model)
+	reader = Reader(fasta=infasta, gff=ingff)
+	region = next(reader)
+	locus = isoform2.Locus(region.name, region.seq, model, limit=inlimit)
 
-# prevs is all the probabilites before labeling
-# posts is all the probabilites after labeling
-# labeling with non-stop or nmd-target multiplies the probability by 1e-3
-prevs = [iso.prob for iso in locus.isoforms]
-posts = []
-rtypes = []
-for iso in locus.isoforms:
-	iso.translate(cds_beg)
-	if   iso.rnatype == 'non-stop': iso.prob *= 1e-3
-	elif iso.rnatype == 'nmd-target': iso.prob *= 1e-3
-	posts.append(iso.prob)
-	rtypes.append(iso.rnatype)
+	with open('prenmdish.gff.tmp', 'w') as fp:
+		locus.write_gff(fp)
+				
+	gene = region.ftable.build_genes()[0]
+	txs = gene.transcripts()
+	atgs = set()
+	for tx in txs:
+		cdss = sorted(tx.cdss, key=lambda x: x.beg)
+		atgs.add(cdss[0].beg -1)
+	cds_beg = sorted(list(atgs))[0]
 
-total = sum([iso.prob for iso in locus.isoforms])
-for iso in locus.isoforms:
-	iso.prob /= total
+	# prevs is all the probabilites before labeling
+	# posts is all the probabilites after labeling
+	# labeling with non-stop or nmd-target multiplies the probability by 1e-3
+	prevs = [iso.prob for iso in locus.isoforms] # don't need to print here
+	posts = []
+	rtypes = []
+	for iso in locus.isoforms:
+		iso.translate(cds_beg)
+		if   iso.rnatype == 'non-stop': iso.prob *= 1e-3
+		elif iso.rnatype == 'nmd-target': iso.prob *= 1e-3
+		posts.append(iso.prob)
+		rtypes.append(iso.rnatype)
+
+	total = sum([iso.prob for iso in locus.isoforms])
+	for iso in locus.isoforms:
+		iso.prob /= total
+		
+	with open('postnmdish.gff.tmp', 'w') as fp:
+		locus.write_gff(fp)
+
+	# compare mdist before and after nmd-ish
+	i1 = isoform2.get_introns(ingff)
+	i2 = isoform2.get_introns('prenmdish.gff.tmp')
+	i3 = isoform2.get_introns('postnmdish.gff.tmp')
+
+	dist1, details1 = isoform2.expdiff(i1, i2)
+	dist2, details2 = isoform2.expdiff(i1, i3)
+
+	info = [region.name, dist1, dist2, dist2-dist1]
+
+	return info
+
+# glob time
+file_pairs = {}
+for fpath in glob.glob(f'{args.smallgenes}/*.gff3'):
+	gid = fpath.split('/')[-1].split('.')[1]
+	file_pairs[gid] = [fpath]
+
+for fpath in glob.glob(f'{args.smallgenes}/*.fa'):
+	gid = fpath.split('/')[-1].split('.')[1]
+	file_pairs[gid].append(fpath)
+
+# i need to parrellize this...
+print(f'gene\tprenmdish\tpostnmdish\tdelta')
+for p in file_pairs:
+	# test only some genes
+	if int(p.split('_')[0]) == 1 and int(p.split('_')[1]) < 20:
+		info = compare_dists(args.model, file_pairs[p][1], file_pairs[p][0], args.limit)
+		print(f'{info[0]}\t{info[1]}\t{info[2]}\t{info[3]}')
 	
-with open('postnmdish.gff.tmp', 'w') as fp:
-	locus.write_gff(fp)
-
-# compare mdist before and after nmd-ish
-i1 = isoform2.get_introns(args.gff)
-i2 = isoform2.get_introns('prenmdish.gff.tmp')
-i3 = isoform2.get_introns('postnmdish.gff.tmp')
-
-dist1, details1 = isoform2.expdiff(i1, i2)
-dist2, details2 = isoform2.expdiff(i1, i3)
-
-print(region.name, dist1, dist2, dist2-dist1)
-
 '''
 python3 nmd-ish.py models/worm.splicemodel ../datacore2024/project_splicing/smallgenes/ch.2_1.fa ../datacore2024/project_splicing/smallgenes/ch.2_1.gff3 --limit 10
 '''
+
 
