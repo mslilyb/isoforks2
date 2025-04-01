6ef5dd7360b2d8ed0a343e5bff7f36d43021fc56
carl <icacedo@ucdavis.edu>
2025-03-31 22:31:15 -0700

    up

--- APCanalysis/run_NMDtester2.py
+++ APCanalysis/run_NMDtester2.py
@@ -11,6 +11,10 @@ parser.add_argument('smallgenes', type=str, metavar='<directory>',
     help='directory with APC .gff3 and .fa files')
 parser.add_argument('model', type=str, metavar='<file>',
     help='splice model file')
+parser.add_argument('--deqn', required=False, type=str, default='dtc',
+	metavar='<string>', help='choose which distance equation to use, '
+	'i.e. dtc (Manhattan), dtx, dty, dkl (Kullback-Leibler), '
+    'or all [%(default)s]')
 parser.add_argument('--limit', required=False, type=int, default=100,
 	metavar='<int>', help='limit number of transcripts [%(default)i]')
 parser.add_argument('--weights', type=str, metavar='<file>',
@@ -28,72 +32,51 @@ for fpath in glob.glob(f'{args.smallgenes}/*.gff3'):
 for fpath in glob.glob(f'{args.smallgenes}/*.fa'):
 	gid = fpath.split('/')[-1].split('.')[1]
 	file_pairs[gid].append(fpath)
-	
-for f in file_pairs:
-	print(f, file_pairs[f])
 
-
-
-
-
-
-
-
-'''
-fpaths = {}
-for file in os.listdir(args.apc_dir):
-    gID = file.split('.')[1]
-    if gID not in fpaths:
-        fpaths[gID] = [(), ()]
-    if file.endswith('.fa'):
-        fpaths[gID][0] = f'{args.apc_dir}{file}'
-    if file.endswith('.gff3'):
-        fpaths[gID][1] = f'{args.apc_dir}{file}'
-
-weights = {}
-with open(args.weights, 'r') as file:
-    for line in file.readlines():
-        line = line.rstrip()
-        if line.startswith('%'): continue
-        line = line.split(',')
-        weights[line[0]] = [x for x in line[1:]]
+if args.weights:
+    weights = {}
+    with open(args.weights, 'r') as file:
+        for line in file.readlines():
+            line = line.rstrip()
+            if line.startswith('%'): continue
+            line = line.split(',')
+            weights[line[0]] = [x for x in line[1:]]
 
 def generate(
-        prog, fasta, model, wacc, wdon, wexs, wins, wexl, winl, winf, limit
+        prog, fasta, model, gff, wacc, wdon, wexs, wins, wexl, winl, winf, 
+        limit, deqn
         ):
 
     cmd = (
-        f'./{prog} {fasta} {model} --wacc {wacc} --wdon {wdon} '
+        f'./{prog} {model} {fasta} {gff} --wacc {wacc} --wdon {wdon} '
         f'--wexs {wexs} --wins {wins} --wexl {wexl} --winl {winl} '
-        f'--winf {winf} --limit {limit}'
+        f'--winf {winf} --limit {limit} --deqn {deqn}'
     )
     cmd = cmd.split(' ')
-    gid = cmd[1].split('.')[-2]
+    gid = cmd[2].split('.')[-2]
     output = subprocess.run(cmd, stdout=subprocess.PIPE, text=True).stdout
-    print(f'working on {gid}...')
+    print(f'{gid} done')
     return output.rstrip()
 
-def worker(input):
-
-    return generate(
-        input[0], input[1], input[2], input[3], input[4],
-        input[5], input[6], input[7], input[8], input[9], input[10]
-    )
-    
+# order of weights
+# fitness,wacc,wdon,wexs,wins,wexl,winl,winf
 inputs = []
-for gID in fpaths:
-    wacc = weights[gID][1]
-    wdon = weights[gID][2]
-    wexs = weights[gID][3]
-    wins = weights[gID][4]
-    wexl = weights[gID][5]
-    winl = weights[gID][6]
-    winf = weights[gID][7]
+for gene in file_pairs:
     input = [
-        args.program, fpaths[gID][0], args.model, wacc, wdon, wexs, wins,
-        wexl, winl, winf, args.limit
+        args.program, file_pairs[gene][1], args.model, file_pairs[gene][0],
+        weights[gene][1], weights[gene][2], weights[gene][3], 
+        weights[gene][4], weights[gene][5], weights[gene][6], 
+        weights[gene][7], args.limit, args.deqn
     ]
     inputs.append(input)
+
+def worker(input):
+    return generate(
+        input[0], input[1], input[2], input[3], input[4], input[5], 
+        input[6], input[7], input[8], input[9], input[10], input[11],
+        input[12]
+     )
+
 
 #s = time.perf_counter()
 with Pool(processes=mp.cpu_count()-1) as pool:
